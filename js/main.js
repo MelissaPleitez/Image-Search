ad58335740a34d7e298c54c98676e3b2896f9146
MelissaPleitez <melissa.ramirez0pleitez@gmail.com>
2023-08-23 17:07:43 -0600

    Adding pagination and functions

--- js/main.js
+++ js/main.js
@@ -6,6 +6,10 @@ function start_App(){
         const form = document.querySelector('#form')
         const result = document.querySelector('#results')
         const searchInfo= document.querySelector('#search')
+        const paginationresult = document.querySelector('#pagination')
+        const pages= 40;
+        let pagination;
+        let iter;
         
         form.addEventListener('submit', submitting_data)
         
@@ -16,7 +20,7 @@ function start_App(){
         const mainVlue= searchInfo.value 
 
         if(mainVlue===''){
-           console.log('Empty Inputs')
+          alert('Empty Inputs')
             return
         }
 
@@ -29,56 +33,127 @@ function start_App(){
       function applying_API(mainVlue){
 
         const API_KEY= '38880226-4562c75a60c21007dbb5e4037'
-        const API=`https://pixabay.com/api/?key=${API_KEY}&q=${mainVlue}`
+        const API=`https://pixabay.com/api/?key=${API_KEY}&q=${mainVlue}&per_page=${pages}`
 
         fetch(API)
             .then(result=> result.json())
-            .then(response=>  creating_html(response.hits) )
+            .then(response=>{
+              pagination= calculate_pages(response.totalHits)
+              creating_html(response.hits)
+            }  
+               )
 
       }
 
       function creating_html(allValues){
-        
+        clean_html(result)
         allValues.forEach(info => {
 
             const {id, previewURL, views, pageURL, largeImageURL, user } = info
 
-            result.innerHTML= `
-            <div class="container">
-            <div class="row">
-            <div class="col-6">
-            <div class="card">
-            <div class="card" style="width: 18rem;">
-            <img src=${largeImageURL} class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title">User: ${user}</h5>
-            <p class="card-text">Views: ${views}</p>
-            <a href=${pageURL} class="btn btn-primary">Page URL</a>
-            </div>
-            </div>
-            </div>
-            </div>
-            </div>
-            </div>
-            
-            `
-            
+            const divContainer = document.createElement('div')
+            divContainer.classList.add('w-100', 'p-3', 'mb-4')
+            const divRow= document.createElement('div')
+            divRow.classList.add('d-fex', 'mt-3', 'justify-content-center')
+            divRow.style.width='48rem'
+            const divCols = document.createElement('div')
+           
+            const divCard = document.createElement('div')
+            divCard.classList.add('card', 'p-3', 'mb-4')
+            const imgContainer = document.createElement('img')
+            imgContainer.classList.add('card-img-top')
+            imgContainer.src= largeImageURL
+            const divBody = document.createElement('div')
+            divBody.classList.add('card-body')
+            const h4Info = document.createElement('h4')
+            h4Info.classList.add('card-title')
+            h4Info.textContent= `Users: ${user} - Views: ${views}`
+            const aInfo = document.createElement('a')
+            aInfo.href= pageURL
+            aInfo.textContent= 'Page URL'
+
+            divBody.appendChild(h4Info)
+            divBody.appendChild(aInfo)
+            divCard.appendChild(imgContainer)
+            divCard.appendChild(divBody)
+            divCols.appendChild(divCard)
+            divRow.appendChild(divCols)
+            divContainer.appendChild(divRow)
+
+            result.appendChild(divRow)
+            showing_pagination()
+           
         });
-
-
-
       }
 
-     
+      function* Calculating_pagination(total){
+        console.log(total)
+        for(let i= 1; i<=total; i++){
+          yield i;
+        }
+      }
+
+
+      function calculate_pages(total_pages){
+        return parseInt(Math.ceil(total_pages/ pages))
+      }
+
+
+      function alert (message){
+        Toastify({
+          text:`${message} ` ,
+          className: "info",
+          duration: 3000, 
+          style: {
+            background: "#9d263a",
+          }
+        }).showToast();
+      }
+
+
+      function clean_html(value){
+        while(value.firstChild){
+          value.removeChild(value.firstChild)
+        }
+      }
+
+      function showing_pagination(){
+        iter = Calculating_pagination(pagination)
+        
+          while(true){
+            const {value, done}= iter.next()
+            if(done) return
+
+            const nav = document.createElement('nav')
+            nav.ariaLabel= 'Page navigation'
+            const ulPage= document.createElement('ul')
+            ulPage.classList.add('pagination')
+            const liPage= document.createElement('li')
+            liPage.classList.add('page-item')
+            const aPage= document.createElement('a')
+            aPage.classList.add('page-link')
+            aPage.textContent= value
+
+            ulPage.appendChild(liPage)
+            liPage.appendChild(aPage)
+            nav.appendChild(ulPage)
+
+            paginationresult.appendChild(nav)
+          }
+        }
 
     }
 
+   
 
+   
     
     
-    
+   
+
     }
 
+    
 
 
 window.addEventListener('DOMContentLoaded', start_App)
